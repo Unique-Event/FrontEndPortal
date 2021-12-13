cb0f53c3e2c9f991833f73172daae48a0e6bfc18
alberttgomes <albertgomes1995@hotmail.com>
2021-12-13 19:22:32 -0300

    att

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ButtonRegister from './ButtonRegister';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
@@ -6,25 +6,9 @@ import './Navbar.css';
 
 export const Navbar = () => {
   const [click, setClick] = useState(false);
-  //const [button, setButton] = useState(true);
   
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
- 
-  // const showButton = () => {
-  //   if (window.innerWidth <= 960) {
-  //     setButton(false);
-  //   } else {
-  //     setButton(true);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   showButton();
-  // }, []);
-
-  //window.addEventListener('resize', showButton);
-
 
   return (
     
